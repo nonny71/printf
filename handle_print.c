5ad051502fae3c8e6ed868ee81162544c381dfbc
nonny71 <your email>
2022-10-21 09:18:04 -0700

    team work

--- handle_print.c
+++ handle_print.c
@@ -14,7 +14,7 @@
  * Return: 1 or 2;
  */
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
-		int flags, int width, int precision, int size)
+	int flags, int width, int precision, int size)
 {
 	int i, unknow_len = 0, printed_chars = -1;
 	fmt_t fmt_types[] = {
@@ -22,6 +22,7 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 		{'i', print_int}, {'d', print_int}, {'b', print_binary},
 		{'u', print_unsigned}, {'o', print_octal},
 		{'x', print_hexadecimal}, {'X', print_hexa_upper},
+		{'p', print_pointer}, {'S', print_non_printable},
 		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
 	};
 	for (i = 0; fmt_types[i].fmt != '\0'; i++)
